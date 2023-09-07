811e14aaa96e05dbae4ed6538e405fdb4d92cf7d
Arthur Halet <arthur.halet@orange.com>
2023-09-07 22:45:46 +0200

    rework tls configuration

--- gsloc/api/config/healthchecks/v1/healthcheck.pb.validate.go
+++ gsloc/api/config/healthchecks/v1/healthcheck.pb.validate.go
@@ -154,7 +154,34 @@ func (m *HealthCheck) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
-	// no validation rules for EnableTls
+	if all {
+		switch v := interface{}(m.GetTlsConfig()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, HealthCheckValidationError{
+					field:  "TlsConfig",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, HealthCheckValidationError{
+					field:  "TlsConfig",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetTlsConfig()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return HealthCheckValidationError{
+				field:  "TlsConfig",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
 
 	switch m.HealthChecker.(type) {
 
@@ -370,6 +397,111 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = HealthCheckValidationError{}
+
+// Validate checks the field values on TlsConfig with the rules defined in the
+// proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *TlsConfig) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on TlsConfig with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in TlsConfigMultiError, or nil
+// if none found.
+func (m *TlsConfig) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *TlsConfig) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Enable
+
+	// no validation rules for Ca
+
+	// no validation rules for ServerName
+
+	if len(errors) > 0 {
+		return TlsConfigMultiError(errors)
+	}
+
+	return nil
+}
+
+// TlsConfigMultiError is an error wrapping multiple validation errors returned
+// by TlsConfig.ValidateAll() if the designated constraints aren't met.
+type TlsConfigMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m TlsConfigMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m TlsConfigMultiError) AllErrors() []error { return m }
+
+// TlsConfigValidationError is the validation error returned by
+// TlsConfig.Validate if the designated constraints aren't met.
+type TlsConfigValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TlsConfigValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TlsConfigValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TlsConfigValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TlsConfigValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TlsConfigValidationError) ErrorName() string { return "TlsConfigValidationError" }
+
+// Error satisfies the builtin error interface
+func (e TlsConfigValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTlsConfig.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TlsConfigValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TlsConfigValidationError{}
 
 // Validate checks the field values on HealthCheckPayload with the rules
 // defined in the proto definition for this message. If any rules are
